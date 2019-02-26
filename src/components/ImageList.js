bd79da336f2f5b9a3b419127aaed63584bfb187e
Keith Cormack <kcormack@definitivelogic.com>
2019-02-25 22:02:49 -0700

    display searched images w/ key

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,11 @@ import React from 'react';
 const ImageList = (props) => {
   console.log(props.images);
 
-  return <div>ImageList</div>
+  const images = props.images.map((image) => {
+    return <img src={image.urls.regular} alt={''} key={image.id} />
+  });
+
+  return <div>{images}</div>;
 };
 
 export default ImageList;
